c66073cd7d4a734629ec3586df20f7ef4744ad40
KAMAL-Kishore <kk7315102@gmail.com>
2026-04-08 00:18:30 +0530

    fix

--- backend/config/sanctum.php
+++ backend/config/sanctum.php
@@ -16,7 +16,7 @@ return [
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
         'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        'http://16.16.78.187/',
+        env('VITE_API_BASE_URL'),
         env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : ''
     ))),
 
